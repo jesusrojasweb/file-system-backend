b7701124bd8170760367f503422ea14f2bfc9a39
Jesus Rojas <jesusrojas.frontend@gmail.com>
2022-08-10 12:08:27 -0400

    Configuracion inicial de los servicios

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -1,16 +1,23 @@
+const fileService = require("../services/fileService");
+
 const getAllFiles = (req, res) => {
+  const allFiles = fileService.getAllFiles();
   res.send(`Get all files`);
 };
 const getOneFile = (req, res) => {
+  const file = fileService.getOneFile(req.params.fileId);
   res.send(`Get file ${req.params.fileId}`);
 };
 const createNewFile = (req, res) => {
+  const createFile = fileService.createNewFile(req.params.fileId);
   res.send(`Get file ${req.params.fileId}`);
 };
 const updateOneFile = (req, res) => {
+  const updatefile = fileService.updateOneFile(req.params.fileId);
   res.send(`Update file ${req.params.fileId}`);
 };
 const deleteOneFile = (req, res) => {
+  fileService.deleteOneFile(req.params.fileId);
   res.send(`Delete file ${req.params.fileId}`);
 };
 
---
+++ src/services/fileService.js
@@ -0,0 +1,23 @@
+const getAllFiles = () => {
+  return;
+};
+const getOneFile = () => {
+  return;
+};
+const createNewFile = () => {
+  return;
+};
+const updateOneFile = () => {
+  return;
+};
+const deleteOneFile = () => {
+  return;
+};
+
+module.exports = {
+  getAllFiles,
+  getOneFile,
+  createNewFile,
+  updateOneFile,
+  deleteOneFile,
+};
